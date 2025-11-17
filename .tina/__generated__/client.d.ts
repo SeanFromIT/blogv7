275e727207ece9c502090b658501502e8c1be68f
Sean Feeney <SeanFromIT@gmail.com>
2025-11-16 21:47:11 -0700

    fix tina update

--- .tina/__generated__/client.d.ts
+++ .tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'D:/Scratch Web/feeney.mba/blogv7/.tina/__generated__/.cache/1755473127891', url: 'https://content.tinajs.io/1.6/content/d644012a-ff5c-432b-8178-266f85acd0a1/github/master', token: '1eae4e97752a1c3d2dd5358a3c741db9057b312d', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '1eae4e97752a1c3d2dd5358a3c741db9057b312d', queries,  });
 export default client;
   
